f798d47570b20ed055665bf998cbe13dc86ac8d5
Jenna <xujenna@gmail.com>
2024-10-21 12:45:44 -0400

    added Sleepbot,  changed order of projects

---
+++ Sleepbot/index.php
@@ -0,0 +1,32 @@
+<!-- text and image -->
+
+<div class="type_test" id="description_text">
+<p>
+The first-line treatment for insomnia is cognitive behavioral therapy for insomnia (CBT-i), which is typically administered in-person over 4-8 weeks. However, trained professionals are few and far between, with an estimated 659 behavioral sleep medicine providers in the US—19% living in California and New York—and many with waitlists of 6 months or more, and out-of-pocket costs averaging $1200 USD.</p>
+<p>
+Several digital versions of CBT-i have been created to address the need for accessibility, but low adherence rates are common, with the vast majority of participants failing to complete entire programs as designed. Additionally, many of these digital services require insurance coverage, employer benefits, and/or appointments, which severely limits access.</p>
+<p>
+Sleepbot codifies CBT-i into a text-based, automated program of daily, just-in-time adaptive interventions (JITAIs) entirely over SMS and WhatsApp. The tight feedback loop of daily self-report and intervention makes it a highly responsive and personalized experience. By leveraging JITAIs over ubiquitous mediums people use every day, Sleepbot makes a notoriously difficult therapeutic protocol easier to adhere to: 80% engagement and 92% retention rates during our 8-week study were drastically higher than others.
+</p>
+<p>
+<img class="img-responsive" style="display:inline-block; width:49%" src="./Sleepbot/texts.png">
+<img class="img-responsive" style="display:inline-block; width:49%" src="./Sleepbot/user_dash.png">
+</p>
+Sleepbot was designed as both a low-threshold standalone service for waitlisted patients, as well as an augment for sleep specialists administering CBT-i. The text messaging service takes care of sleep diaries for both clinicians and their patients, performing calculations for the former and ensuring adherence from the latter. The clinician dashboard is optimized for quick review—highlighting long-term trends and potentially problematic metrics while also allowing providers to drill down into summarized data when more granularity is desired—enabling providers to monitor their entire caseload.
+</p>
+<p>
+<img class="img-responsive" src="./Sleepbot/clinician_dash.png">
+</p>
+</div>
+<script>
+
+for (i=0; i<pngs.length; i++) {
+	$('#img-container').append("<p><img class='img-responsive' src='" + pngs[i] + ".png'> </p>");
+}
+
+for (i=0; i<jpgs.length; i++) {
+	$('#img-container').append("<p><img class='img-responsive' src='" + jpgs[i] + ".jpg'> </p>");
+}
+
+</script> 
+<!-- end text and image -->
