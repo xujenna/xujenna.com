93ba19e2abf0dafbe4e52a27a5ac44aeaea3c8fc
Jenna <xujenna@gmail.com>
2024-11-06 16:21:22 -0500

    hey coach case study

--- HeyCoach/index.php
+++ HeyCoach/index.php
@@ -1,19 +1,72 @@
 <!-- text and image -->
-
+<p>
+Following a pivot to youth sports, Optimism Health’s founding team brought me on for three months as their first design hire to overhaul the app for hockey players aged 13-18. The original experience was built for data-driven health optimizers and offered little appeal for teenage users. Opti, their AI assistant coach, especially struggled to engage them in conversation. With only two engineers and myself as the sole designer, the challenge was to deliver a high-impact redesign before hockey season began in three months.
+</p>
+<p>
+<b>Goals:</b> Revamp the app to boost engagement with youth athletes in order to demonstrate value to their coaches—the customer—and build on the momentum of their initial traction in this new market. To make the most of limited time and resources, we prioritized strategic UX enhancements and lightweight visual updates, with a full rebrand planned for a later phase.
+</p>
+<h2 class="section_hed">Process</h2>
 <div class="type_test" id="description_text">
 <p>
-	Leading a redesign, rebrand, and product rethink of Hey Coach's (fka Optimism Health) experience following a pivot to youth athletes. More to come!
+<b>User Research</b>
+</p><p>
+	I conducted interviews to understand youth athletes’ motivations and pain points, revealing several key insights:
+<ul>
+	<li>
+		All players cited being motivated by seeing improvement in their own personal fitness, expressing desire to <b>“be their best selves”</b>.
+	</li>
+	<li>
+		All players cited their team as major motivators in keeping up with on- and off-ice fitness regimens, and valued the <b>sense of community and connection</b> their team provided. Interestingly, they all expressed interest in <b>friendly competition</b> with their teammates as well.
+	</li>
+	<li>
+		Despite insisting that Opti was helpful, many players skipped conversations with the AI coach, blaming repetitive, bland topics as well as their own <b>perceived impatience and disinterest</b>.
+	</li>
+	<li>
+		Despite the survey-like format, <b>players treated check-ins more like checklists</b>, often returning to the app throughout the day to log progress. This misalignment highlighted the need for a new interface that matched user behavior.
+	</li>
+</ul>
+</p>
+<p>
+<b>Ideation & Refinement</b>
+</p><p>Because interviews were scheduled on a rolling basis, I started by designing low-hanging usability improvements in high fidelity to seed discussions with our founding team. Eventual UXR findings enabled me to prioritize and develop new and improved features, which I expressed with high-fidelity mockups in Figma to ensure productive conversations with the founding team (who, by their own admission, knew nothing about design!). This also allowed us to better negotiate resource constraints and plan for a phased rollout with maximum impact.
+</p>
+<h2 class="section_hed">Final Designs</h2>
+<div class="type_test" id="description_text">
+<p>
+<b>Usability improvements</b>
+</p><p>The existing web app, built by the founders, had limited functionality and posed usability challenges, such as small fonts, non-standard form inputs, and a pop-up chat window for Opti that made interactions cumbersome.<br>
+<img class="img-responsive" src="./usability.png"><br>
+</p>
+<p>
+<b>A homescreen that mirrors your best self</b>
+</p><p>
+What’s more motivating than a good old visualization? When users now login, they immediately see their personal records as a reminder of their own potential—as well as an incentive to complete their check-in! I also introduced more forgiving check-in streaks, which allow users to claw back their streaks by retroactively completing check-ins.<br>
+<img class="img-responsive" src="./homescreen.png"><br>
+</p>
+<p>
+<b>Leveraging the team to improve the player</b>
+</p><p>I introduced team-branded skins as an easy way to tap into a major user motivator (community) while punting on visual direction (knowing a branding exercise was coming). Additionally, subtle leaderboards, as well as the presence of teammate profiles, added easy elements of healthy competition.<br>
+<img class="img-responsive" src="./team_skins.png"><br>
+</p>
+<p>
+<b>A new task management paradigm</b>
+</p><p>
+As a north-star design, I proposed replacing the survey-style check-in with a task management system where players could check off tasks throughout the day, making it easier to track and log activities as they occurred. This shift aligned with how players naturally interacted with the app, providing a more intuitive experience and fostering adherence and habit formation.<br>
+<img class="img-responsive" src="./task_paradigm.png"><br>
+</p>
+<p>
+<b>A new Opti</b>
+</p><p>As part of the new experience, I developed a content strategy for Opti to start conversations with tailored prompt variations based on players’ behaviors, values, and interests. I proposed separating conversations from the check-in flow, enabling Opti to deliver timely, asynchronous messages triggered by user behavior instead of check-in completion.<br>
+<img class="img-responsive" src="./async_opti.png"><br>
 </p>
 </div>
-<script>
-
-for (i=0; i<pngs.length; i++) {
-	$('#img-container').append("<p><img class='img-responsive' src='" + pngs[i] + ".png'> </p>");
-}
-
-for (i=0; i<jpgs.length; i++) {
-	$('#img-container').append("<p><img class='img-responsive' src='" + jpgs[i] + ".jpg'> </p>");
-}
-
-</script> 
+<h2 class="section_hed">Implementation & Outcome</h2>
+<div class="type_test" id="description_text">
+	<p>
+We rolled out the top three items in time for the start of hockey season, while doing ongoing testing with Opti’s conversation starters. The task management system and Opti’s asynchronous communications are scheduled for implementation in Q4.
+</p>
+<p>
+While still fresh, key outcomes have included increased daily engagement among existing users and significantly increased traction with the business’s new target customer. The redesigned app was used in demos with interested coaches and drove the acquisition of 10+ new teams and counting. Existing users reported significantly favoring the redesign as well.
+</p>
+</div>
 <!-- end text and image -->
